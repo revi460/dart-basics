3e75e26ea577ad3f524e7bcab231ed0480bc698f
SMP IT ASSAIDIYYAH <revishoop006@gmail.com>
2024-03-10 22:59:19 +0700

    integer

---
+++ dart_project/bin/01_string.dart
@@ -0,0 +1,4 @@
+void main (){
+  String data ="hallo pemrograman mobile";
+  print (data.toUpperCase());
+}
---
+++ dart_project/bin/02_num.dart
@@ -0,0 +1,7 @@
+void main (){
+  num data = 20;
+  print (data);
+  num data2= 22.2;
+  print (data);
+  
+}
